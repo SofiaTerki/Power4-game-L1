4bcedd712dc4646fbe9de2242b7d37a4f9ecf546
Sofia TERKI <sofia.terki@ens.uvsq.fr>
2022-05-03 16:17:59 +0200

    ajout des fonctions charger et sauvegarder (faites par Clémence) + bouton démarrer

--- puissance_4.py
+++ puissance_4.py
@@ -13,6 +13,7 @@ import tkinter as tk
 NOMBRE_LIGNE = 6
 NOMBRE_COLONNE = 7
 DIAMETRE_JETON = 100
+start = True
 
 # Variables globals
 configuration = []
@@ -54,25 +55,25 @@ def affichage_jeuton():
 def mouvement_jeton(event):
     """Fonction qui permet de faire tomber le jeton dans la
     grille jusqu'à toucher le fond ou un autre jeton"""
-    global joueur, configuration
+    global joueur, configuration, column
     x = event.x
     ligne = -1
-    
+    column =[0, 1, 2, 3, 4, 5, 6]
     #Détermination de la colonne sur laquelle le joueur a cliqué
     if x<100:
-        colonne = 0
+        colonne = column[0]
     if 100<x<200:
-        colonne = 1
+        colonne = column[1]
     if 200<x<300:
-        colonne = 2
+        colonne = column[2]
     if 300<x<400:
-        colonne = 3
+        colonne = column[3]
     if 400<x<500:
-        colonne = 4
+        colonne = column[4]
     if 500<x<600:
-        colonne = 5
+        colonne = column[5]
     if 600<x<700:
-        colonne = 6
+        colonne = column[6]
         
     while configuration[ligne][colonne] != 0:
         ligne -= 1
@@ -109,18 +110,67 @@ def determination_du_gagnant():
     #match nul
     if 0 not in configuration and winner == False:
         print("Manche nulle")
-
+    #bloquer la colonne qui est déjà pleine
     
+
+def sauvegarde():
+    nom_fichier = input("Nom du fichier à sauvegarder")
+    fichier_sauvegarde = open(nom_fichier+".txt","w")
+    for i in configuration:
+        for j in i:
+            fichier_sauvegarde.write(str(j))
+        fichier_sauvegarde.write("\n")
+    fichier_sauvegarde.close
+
+def charger():
+    global configuration
+    nom_fichier = input("Nom du fichier à charger")
+    fichier_charger = open(nom_fichier+".txt","r")
+    config = []
+    for ligne in fichier_charger:
+        config.append(list(ligne))
+    print(config)
+    print(type(config))
+    print(type(config[2][3]))
+    for i in config:
+        del(i[-1])
+    for i in range(NOMBRE_LIGNE):
+        for j in range(NOMBRE_COLONNE):
+            config[i][j]=int(config[i][j])
+    print(config)
+    print(type(config))
+    print(type(config[2][3]))    
+    fichier_charger.close
+    configuration = config
+    affichage_jeuton()
+
+def demarrer():
+    configuration_initiale()
+    affichage_jeuton()
+
 # Affichage graphique
 
 racine = tk.Tk()
 canvas = tk.Canvas(racine, width=DIAMETRE_JETON*NOMBRE_COLONNE, height=DIAMETRE_JETON*NOMBRE_LIGNE, bg="blue")
 
+#création des wigdests
+bouton_sauvegarder = tk.Button(racine, text="sauvegarde", command=lambda : sauvegarde())
+bouton_charger = tk.Button(racine, text="charge", command=lambda : charger())
+bouton_demarrer = tk.Button(racine, text = "démarrer", command = demarrer)
+
+# Placement des widgets
+canvas.grid(column=0,row=0, rowspan=2)
+bouton_sauvegarder.grid(column=1, row=0)
+bouton_charger.grid(column=1, row=1)
+bouton_demarrer.grid(column=1, row = 2)
 canvas.grid()
 
+#liaison d'événements 
 canvas.bind('<Button>', mouvement_jeton )
-configuration_initiale()
-affichage_jeuton()
 
+# fonctions appliquées avant le démarrage de la fenêtre graphique
+#configuration_initiale()
+#affichage_jeuton()
 
+# Lancement de l'interface graphique
 racine.mainloop()
